f8fc93f68f54b017eef323ee33b52e2882850d62
DmitrySobol1 <saibleonetwo@gmail.com>
2025-02-16 19:40:53 +0300

    16-02 cnang08

--- friend.js
+++ friend.js
@@ -40,6 +40,6 @@ document.addEventListener("click", function (event) {
 
 document.getElementById('refbtn').addEventListener('click',()=>{
     console.log ('ref btn clicked')
-    const inviteLink = "https://t.me/your_bot?start=777";
-    window.Telegram.WebApp.openTelegramLink(`tg://msg_url?url=${encodeURIComponent(inviteLink)}`);
+    // const inviteLink = "https://t.me/your_bot?start=777";
+    window.Telegram.WebApp.openTelegramLink('https://t.me/easydev_school');
 })
